a6c37f503dc2939368430e4ff2970fcf663dab2a
EunJung <venare36@naver.com>
2020-10-10 22:36:15 +0900

    10/10

---
+++ src/baekjoon/ex1550.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class ex1550 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String num = sc.next();
+		System.out.println(Integer.parseInt(num, 16));
+		
+	}
+
+}
